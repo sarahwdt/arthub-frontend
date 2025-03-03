d3a9b6702682e6632b63d2b60c276272b6e1205b
Sarah <sarahwilldietoday@gmail.com>
2025-03-03 18:04:06 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<template w-full="100%">
+  <v-container fluid>
+    <v-row justify="center">
+      <v-col cols-lg="8" cols-md="10" cols-sm="12">
+        <v-tabs align-tabs="center">
+          <v-tab :value="1">Halo</v-tab>
+          <v-tab :value="2">2</v-tab>
+          <v-tab :value="3">End...</v-tab>
+        </v-tabs>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style>
+</style>
